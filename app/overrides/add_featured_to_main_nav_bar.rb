b164060d0dab39797314287614e35f8062b49c3f
pholls <pholls@users.noreply.github.com>
2017-06-29 13:30:22 -0700

    edit Featured main nav bar link

--- app/overrides/add_featured_to_main_nav_bar.rb
+++ app/overrides/add_featured_to_main_nav_bar.rb
@@ -3,5 +3,5 @@ Deface::Override.new(virtual_path: 'spree/shared/_main_nav_bar',
   insert_after: "erb[loud]:contains('link_to Spree.t(:home)')",
   text: "
     </li>
-    <li id='featured-link' data-hook><%= link_to Spree.t(:featured), spree.featured_path %>
+    <li id='featured-link' data-hook><%= link_to 'Featured Products', spree.featured_path %>
 ")
