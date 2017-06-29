3ced8ec70cfc067d3be232541b335394136357e9
pholls <pholls@users.noreply.github.com>
2017-06-29 12:00:39 -0700

    select featured products

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
   HomeController.class_eval do
     def featured
-      @products = Product.joins(:variants_including_master).where('spree_variants.featured').uniq
+      @products = Product.joins(:variants_including_master).where('"spree_variants"."featured" = "t"').uniq
     end
   end
 end
